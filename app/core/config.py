291f579e4558340da854f0fafbe3111fa8b3bd01
vivek_mahajan <vivekmahajan2001vv@gmail.com>
2026-06-18 17:38:51 +0530

    Updated config settings

--- app/core/config.py
+++ app/core/config.py
@@ -14,14 +14,12 @@ class Settings(BaseSettings):
     SQLALCHEMY_ECHO: bool = True
     API_V1_PREFIX: str = "/api/v1"
 
-    DATABASE_URL: str = "mysql+aiomysql://root:Mahakal12_12@127.0.0.1:3306/NesaCare"
-    DATABASE_URL_SYNC: str = "mysql+pymysql://root:Mahakal12_12@127.0.0.1:3306/NesaCare"
 
 
     # DATABASE_URL: str = "mysql+aiomysql://root:root@localhost/NesaCare"
     # DATABASE_URL_SYNC: str = "mysql+pymysql://root:root@localhost/NesaCare"
-   # DATABASE_URL: str = "mysql+aiomysql://nexauser:nexa123@localhost/nexacare"
-   # DATABASE_URL_SYNC: str = "mysql+pymysql://nexauser:nexa123@localhost/nexacare"
+    DATABASE_URL: str = "mysql+aiomysql://nexauser:nexa123@localhost/nexacare"
+    DATABASE_URL_SYNC: str = "mysql+pymysql://nexauser:nexa123@localhost/nexacare"
 
 
     SECRET_KEY: str = "change-me"

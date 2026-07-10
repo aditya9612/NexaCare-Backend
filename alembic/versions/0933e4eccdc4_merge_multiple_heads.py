7d35662d4c23834457ccaf1e073df99fcb6495f0
Pratik Narkhede <narkhedepratik1@gmail.com>
2026-07-10 12:45:11 +0530

    fix the multiple head s

--- alembic/versions/0933e4eccdc4_merge_multiple_heads.py
+++ alembic/versions/0933e4eccdc4_merge_multiple_heads.py
@@ -1,8 +1,8 @@
 """merge_multiple_heads
 
-Revision ID: 21449ef446a4
+Revision ID: 0933e4eccdc4
 Revises: 0ee4d206a321, 54940cc7f704
-Create Date: 2026-07-10 10:57:19.903895
+Create Date: 2026-07-10 12:38:29.650769
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 
-revision: str = '21449ef446a4'
+revision: str = '0933e4eccdc4'
 down_revision: Union[str, None] = ('0ee4d206a321', '54940cc7f704')
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
